54da1f7ec2283a31991ec66f7dfbb102f77684e7
Nicolas De Palma <wellcomenicolas@gmail.com>
2022-05-19 15:21:10 +0200

    Initial Page

---
+++ index.php
@@ -0,0 +1,30 @@
+<!-- Descrizione:
+Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall'utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
+
+<?php
+
+$paragraph = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, numquam ab at facere ratione quod corrupti minima necessitatibus quidem. Unde aperiam aliquid odit repellendus ducimus nisi numquam, culpa eligendi autem?";
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
